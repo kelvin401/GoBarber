e20bce4d18e901d7bb79e8bc62e61a8e4331710b
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2020-07-09 23:33:33 -0300

    Create route to update profile of user

---
+++ backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
+
+export default class ProfileController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { user_id } = request.body;
+
+    const ShowProfile = container.resolve(ShowProfileService);
+
+    const user = await ShowProfile.execute({ user_id });
+
+    delete user.password;
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { name, email, old_password, password } = request.body;
+
+    const updateProfile = container.resolve(UpdateProfileService);
+
+    const user = await updateProfile.execute({
+      user_id: request.user.id,
+      name,
+      email,
+      old_password,
+      password,
+    });
+
+    delete user.password;
+
+    return response.status(200).json(user);
+  }
+}
