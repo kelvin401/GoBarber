e8af1c84c14ba0fa13ff2196cdb8113b87bcf1c0
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2020-07-14 23:55:40 -0300

    Application test of availability providers

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,14 +2,28 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '../controllers/ProvidersController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController.ts';
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController.ts';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 // Application the Middleware
 providersRouter.use(ensureAuthenticated);
 
 // List Providers
 providersRouter.get('/', providersController.index);
+
+// Availability
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
